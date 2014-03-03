5d091cb6fca90a738a02ef370928719c1450859d
Helder <he7d3r@gmail.com>
2014-03-03 19:00:57 +0000

    Allow undoing parts of a change by clicking in the parts of a diff
    Note: this code was originally hosted on Portuguese Wikibooks (https://pt.wikibooks.org)

---
+++ src/UndoFromDiff.js
@@ -0,0 +1,46 @@
+/**
+ * Allow undoing parts of a change by clicking in the parts of a diff
+ * @author: [[User:Helder.wiki]]
+ * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/UndoFromDiff.js]] ([[File:User:Helder.wiki/Tools/UndoFromDiff.js]])
+ */
+/*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, onevar: true */
+/*global jQuery, mediaWiki */
+( function ( mw, $ ) {
+'use strict';
+
+function undo( e ){
+	var $row = $( e.target ).closest( 'tr' ),
+		oldText = $row.find( 'td.diff-deletedline' ).text(),
+		newText = $row.find( 'td.diff-addedline' ).text(),
+		$box = $( '#wpTextbox1' ),
+		text = $box.val(),
+		reNewText;
+	// console.log( 'oldText', oldText );
+	// console.log( 'newText', newText );
+	if( !newText ){
+		alert( 'The script is not able to undo this change (yet!).' );
+		return;
+	}
+	reNewText = new RegExp( $.escapeRE( newText ) ); // , 'g'
+/*	console.log( 'reNewText', reNewText );
+	var match = text.match( newText )
+	console.log( 'match', match );
+	console.log( 'match.length', match.length );
+	if( match.length !== 1 ){
+		alert( 'The script is not able to undo this change (yet!).' );
+		return;
+	}*/
+	$box.val( text.replace( reNewText, oldText ) );
+	$( '#wpDiff' ).click();
+}
+
+if( $.inArray( mw.config.get( 'wgAction' ), [ 'edit', 'submit' ] ) !== -1 ){
+	$( function(){
+		var $changes = $( 'table.diff' ).find( '.diff-deletedline, .diff-addedline' );
+		if( $changes.length ){
+			$changes.dblclick( undo );
+		}
+	} );
+}
+
+}( mediaWiki, jQuery ) );
