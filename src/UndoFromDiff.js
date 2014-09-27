0a2ae39fcd92836ba7a1cf2b2b28d297a14f482e
Helder <he7d3r@gmail.com>
2014-09-27 20:49:03 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/UndoFromDiff.js
+++ src/UndoFromDiff.js
@@ -4,43 +4,43 @@
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
 ( function ( mw, $ ) {
-'use strict';
+	'use strict';
 
-function undo( e ){
-	var $row = $( e.target ).closest( 'tr' ),
-		oldText = $row.find( 'td.diff-deletedline' ).text(),
-		newText = $row.find( 'td.diff-addedline' ).text(),
-		$box = $( '#wpTextbox1' ),
-		text = $box.val(),
-		$diffButton,
-		match,
-		reNewText;
-	if ( !e.ctrlKey ) {
-		return;
+	function undo( e ) {
+		var $row = $( e.target ).closest( 'tr' ),
+			oldText = $row.find( 'td.diff-deletedline' ).text(),
+			newText = $row.find( 'td.diff-addedline' ).text(),
+			$box = $( '#wpTextbox1' ),
+			text = $box.val(),
+			$diffButton,
+			match,
+			reNewText;
+		if ( !e.ctrlKey ) {
+			return;
+		}
+		if ( !newText ) {
+			alert( 'The script is not able to undo this change (yet!).' );
+			return;
+		}
+		reNewText = new RegExp( $.escapeRE( newText ), 'g' );
+		match = text.match( reNewText );
+		if ( match.length !== 1 ) {
+			alert( 'This text appears more than once in the page, so it is safer to fix it manually.' );
+			return;
+		}
+		$box.val( text.replace( reNewText, oldText ) );
+		// Support for [[w:en:User:Js/ajaxPreview]]
+		$diffButton = $( '#wpDiffLive' );
+		if ( !$diffButton.length ) {
+			$diffButton = $( '#wpDiff' );
+		}
+		$diffButton.click();
 	}
-	if( !newText ){
-		alert( 'The script is not able to undo this change (yet!).' );
-		return;
-	}
-	reNewText = new RegExp( $.escapeRE( newText ), 'g' );
-	match = text.match( reNewText );
-	if( match.length !== 1 ){
-		alert( 'This text appears more than once in the page, so it is safer to fix it manually.' );
-		return;
-	}
-	$box.val( text.replace( reNewText, oldText ) );
-	// Support for [[w:en:User:Js/ajaxPreview]]
-	$diffButton = $( '#wpDiffLive' );
-	if( !$diffButton.length ){
-		$diffButton = $( '#wpDiff' );
-	}
-	$diffButton.click();
-}
 
-if( $.inArray( mw.config.get( 'wgAction' ), [ 'edit', 'submit' ] ) !== -1 ){
-	$( function(){
-		$( '#mw-content-text' ).on( 'dblclick', '.diff-deletedline, .diff-addedline', undo );
-	} );
-}
+	if ( $.inArray( mw.config.get( 'wgAction' ), [ 'edit', 'submit' ] ) !== -1 ) {
+		$( function () {
+			$( '#mw-content-text' ).on( 'dblclick', '.diff-deletedline, .diff-addedline', undo );
+		} );
+	}
 
 }( mediaWiki, jQuery ) );
